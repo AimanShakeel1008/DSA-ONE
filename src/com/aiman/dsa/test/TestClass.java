3a0c07d80d9c7ae274bc584452d4573476a0b802
AimanShakeel1008 <aimanshakeel1008@gmail.com>
2022-05-21 12:39:01 +0530

    First Commit. Happy Coding!

---
+++ src/com/aiman/dsa/test/TestClass.java
@@ -0,0 +1,10 @@
+package com.aiman.dsa.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Creating repository for DSA-ONE project.");
+		System.out.println("Happy Coding!");
+	}
+
+}
